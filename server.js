99a441c51e323e33ac50518f318accd1aa8c0ed7
Abhilash Patel <abhilashpatel687@gmail.com>
2022-10-14 04:15:26 +0530

    payment test

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+const razorpay = require("razorpay");
+app.use(bodyParser.json());
+
+var instance = new razorpay({
+  key_id: "rzp_test_KFFAcc8lPfLdwf",
+  key_secret: "mD7tBM34NToGxmPCQ3HCingU",
+});
+
+
+app.get("/", (req, res) => {
+res.sendFile('index.html', {root: __dirname });
+
+});
+app.post("/order/orderId", (req, res) => {
+console.log("create order request",req.body);
+var options = {
+    amount: req.body.amount,   // amount in the smallest currency unit
+    currency: "INR",
+    receipt: "rcp1",
+  };
+  instance.orders.create(options, async function (err, order) {
+    console.log(order);
+    res.send({ orderId: order.id });
+  });
+})
+
+app.post("/api/payment/verify",(req,res)=>{
+
+    let body=req.body.response.razorpay_order_id + "|" + req.body.response.razorpay_payment_id;
+   
+     var crypto = require("crypto");
+     var expectedSignature = crypto.createHmac('sha256', '<YOUR_API_SECRET>')
+                                     .update(body.toString())
+                                     .digest('hex');
+                                     console.log("sig received " ,req.body.response.razorpay_signature);
+                                     console.log("sig generated " ,expectedSignature);
+     var response = {"signatureIsValid":"false"}
+     if(expectedSignature === req.body.response.razorpay_signature)
+      response={"signatureIsValid":"true"}
+         res.send(response);
+     });
+   
+   app.listen(3000, () => {
+     console.log(`Example app listening at http://localhost:3000`)
+   })
+
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  var title = "Home";
+  var subTitle = "This is set to the var named subTitle.";
+  res.render("page", { title, subtitle });
+});
+// app.listen(3000);
+console.log("Server has successfully started.");
